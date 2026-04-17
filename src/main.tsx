0731b4ab92fbe7bd5f75dfebd0f0dd97a255d013
Ananya <ananyachella.edu@gmaul.com>
2026-04-18 00:34:25 +0530

    fix rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
->>>>>>> 5095371b73410f173a25e975ba85a1b541e5fc48
